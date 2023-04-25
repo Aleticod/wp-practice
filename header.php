3ce6734a218dfaf23b825e4d8cd594ebc0c44763
Aleticod <193837@unsaac.edu.pe>
2023-04-25 22:09:08 +0000

    Add logo into header page

--- header.php
+++ header.php
@@ -7,4 +7,13 @@
     <?php wp_head() ?>
 </head>
 <body>
-    
+
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <img src="<?php echo get_template_directory_uri().'/assets/img/logo.png' ?>" alt="logo platzi gif">
+            </div>
+        </div>
+    </div>
+</header>
